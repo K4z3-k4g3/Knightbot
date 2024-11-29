1e7fda1886cd002bce1ea6d8c3807263852952bb
Jael <155875950+K4z3-k4g3@users.noreply.github.com>
2024-11-29 06:10:13 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'My Sticker Pack',
-  author: 'My Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  packname: 'HE IS',
+  author: 'JAEL',
+  botName: "JAEL-MD",
+  botOwner: 'JAEL', // Your name
+  ownerNumber: '2348117974849', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
